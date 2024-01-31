8d083059f381aeb6106c9e334f6035b26c5a28e0
dpayne <darby.payne@gmail.com>
2024-01-30 18:24:31 -0800

    Switching color schemes

--- init.lua
+++ init.lua
@@ -20,5 +20,7 @@ if vim.env.SSH_CLIENT then
     vim.cmd([[colorscheme gruvbox]])
     vim.cmd([[hi Normal guibg=NONE ctermbg=NONE]])
 else
-    vim.cmd([[colorscheme pywal16]])
+    -- vim.cmd([[colorscheme pywal16]])
+    vim.cmd([[colorscheme gruvbox]])
+    vim.cmd([[hi Normal guibg=NONE ctermbg=NONE]])
 end
